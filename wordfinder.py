c9e038d9fba0f6f17f533df4381429cfcbf0f543
Regina Steffen <reginasteffen@live.com>
2024-06-01 17:12:39 -0500

    finished assignment - implemented random method

--- wordfinder.py
+++ wordfinder.py
@@ -1,17 +1,21 @@
 """Word Finder: finds random words from a dictionary."""
-
+from random import choices
 
 class WordFinder:
 
     def __init__(self, file):
         dictionary = open(file)
         self.words = self.parse(dictionary)
+        dictionary.close()
         print(f"{len(self.words)} words read")
+
+        #close file after reading
 
     def parse(self, dictionary):
         return [w.strip() for w in dictionary]
     
     def random(self):
+        return choices(self.words)
 
 
 wf = WordFinder("words.txt")
